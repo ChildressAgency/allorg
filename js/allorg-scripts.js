637d5295020cd7efc7534e5ef76344a6779e5b50
childressdev <jcampbell@childressagency.com>
2017-10-31 16:07:28 -0400

    home demo header styled

---
+++ js/allorg-scripts.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function($){
+  $('.ico-search').on('click', function(e){
+    e.preventDefault();
+    //$('.header-search-form').css('display', 'block');
+    $('.header-search-form').show('fast', 'linear');
+  });
+  $('.dismiss-search-form').on('click', function(){
+    $('.header-search-form').hide('fast', 'linear');
+  });
+  $(document).mouseup(function(e){
+    var container = $('.header-search-form');
+    if(!container.is(e.target) && container.has(e.target).length ===0){
+      container.hide('fast', 'linear');
+    }
+  })
+});
