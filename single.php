bc62fc473186c08377f94da4994c480c58a031f9
childressdev <jcampbell@childressagency.com>
2017-11-09 14:49:16 -0500

    change breadcrumb section to template part

--- single.php
+++ single.php
@@ -1,20 +1,5 @@
 <?php get_header(); ?>
-  <div class="container">
-    <div class="breadcrumbs" typeof="BreadcrumbList" vocab="https://schema.org/">
-      <?php if(function_exists('bcn_display')){
-        bcn_display();
-      } ?>
-    </div>
-    <div class="print-share">
-      <a href="#" onclick="window.print();"><i class="fa fa-print"></i></a>
-      <?php if(function_exists('ADDTOANY_SHARE_SAVE_KIT')){
-        ADDTOANY_SHARE_SAVE_KIT(array('use_current_page' => true));
-      } ?>
-      <?php if(function_exists('ADDTOANY_SHARE_SAVE_KIT')){
-        ADDTOANY_SHARE_SAVE_KIT(array('buttons' => array('email')));
-      } ?>
-    </div>
-  </div>
+<?php get_template_part('partials/breadcrumb', 'section'); ?>
 <main id="main">
   <div class="container narrow">
     <article>
