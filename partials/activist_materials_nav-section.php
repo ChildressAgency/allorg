f4be6d7b2146ed067af82b1405efcf8689ef2cf9
childressdev <jcampbell@childressagency.com>
2017-11-14 10:20:13 -0500

    added activist materials top nav

--- partials/activist_materials_nav-section.php
+++ partials/activist_materials_nav-section.php
@@ -1,63 +1,63 @@
-  <nav id="top-nav">
-    <div class="container">
+<nav id="top-nav">
+  <div class="container">
       <div class="row">
         <div class="col-sm-4">
           <?php
-            $learn_nav_left_args = array(
-              'theme_location' => 'learn-nav-left',
-              'menu' => 'Learn Navigation Left',
+            $activist_materials_nav_left_args = array(
+              'theme_location' => 'activist-materials-nav-left',
+              'menu' => 'Activist Materials Navigation Left',
               'container' => '',
               'container_class' => '',
               'container_id' => '',
               'menu_class' => 'nav top-nav',
               'menu_id' => '',
               'echo' => true,
-              'fallback_cb' => 'allorg_learn_nav_left_fallback',
+              //'fallback_cb' => 'allorg_activist_materials_nav_left_fallback',
               'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
               'depth' => 2,
               'walker' => new wp_bootstrap_navwalker()
             );
-            wp_nav_menu($learn_nav_left_args);
+            wp_nav_menu($activist_materials_nav_left_args);
           ?>  
         </div>
         <div class="col-sm-4">
           <?php
-            $learn_nav_center_args = array(
-              'theme_location' => 'learn-nav-center',
-              'menu' => 'Learn Navigation Center',
+            $activist_materials_nav_center_args = array(
+              'theme_location' => 'activist-materials-nav-center',
+              'menu' => 'Activist Materials Navigation Center',
               'container' => '',
               'container_class' => '',
               'container_id' => '',
               'menu_class' => 'nav top-nav',
               'menu_id' => '',
               'echo' => true,
-              'fallback_cb' => 'allorg_learn_nav_center_fallback',
+              //'fallback_cb' => 'allorg_activist_materials_nav_center_fallback',
               'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
               'depth' => 2,
               'walker' => new wp_bootstrap_navwalker()
             );
-            wp_nav_menu($learn_nav_center_args);
+            wp_nav_menu($activist_materials_nav_center_args);
           ?>
         </div>
         <div class="col-sm-4">
           <?php
-            $learn_nav_right_args = array(
-              'theme_location' => 'learn-nav-right',
-              'menu' => 'Learn Navigation Right',
+            $activist_materials_nav_right_args = array(
+              'theme_location' => 'activist-materials-nav-right',
+              'menu' => 'Activist Materials Navigation Right',
               'container' => '',
               'container_class' => '',
               'container_id' => '',
               'menu_class' => 'nav top-nav',
               'menu_id' => '',
               'echo' => true,
-              'fallback_cb' => 'allorg_learn_nav_right_fallback',
+              //'fallback_cb' => 'allorg_activist_materials_nav_right_fallback',
               'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
               'depth' => 2,
               'walker' => new wp_bootstrap_navwalker()
             );
-            wp_nav_menu($learn_nav_right_args);
+            wp_nav_menu($activist_materials_nav_right_args);
           ?>
         </div>
       </div>
-    </div>
-  </nav>
+  </div>
+</nav>
