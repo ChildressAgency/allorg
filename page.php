f4ca06c65ef6605a8111c6e2e18f09e8ab337325
childressdev <jcampbell@childressagency.com>
2018-01-17 13:53:24 -0500

    added title to nav pages

--- page.php
+++ page.php
@@ -17,6 +17,9 @@
   <div class="container narrow">
     <article>
       <?php if(have_posts()): while(have_posts()): the_post(); ?>
+        <?php if($parent->post_name == 'learn' || $parent->post_name == 'activist-materials'): ?>
+          <h1 class="text-center"><?php the_title(); ?></h1>
+        <?php endif; ?>
         <?php the_content(); ?>
       <?php endwhile; endif; ?>
     </article>
